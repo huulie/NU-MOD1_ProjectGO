1daef02d3ba2be159fe8e2b6c2eedadf9294fbf9
Huub Lievestro <huub.lievestro@nedap.com>
2020-01-26 20:32:43 +0100

    Updated generated JavaDoc

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"goGame"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"goStrategy"}]
